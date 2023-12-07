4b46f0346e8faf86a21144c6c44e48005d4ea3fd
Yash Verma <yashv1752@gmail.com>
2023-12-07 12:01:30 +0530

    config pratice completed started working on project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import userRouter from "./routes/user.js"
+import {config} from "dotenv"
 
 export const app = express();
 
+config({
+    path:"./data/config.env"
+})
 
 app.use(express.json())
 app.use("/user",userRouter)
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ import { connectDB } from "./data/database.js";
 connectDB()
 
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is working")
 })
