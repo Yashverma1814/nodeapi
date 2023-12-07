90a0e6dcf0430aa251a44f74eec58bcc74604f7e
Yash Verma <yashv1752@gmail.com>
2023-12-07 11:45:09 +0530

    database has been seprated from app and server files

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
 import userRouter from "./routes/user.js"
 
-const app = express();
+export const app = express();
 
 
 app.use(express.json())
 app.use("/user",userRouter)
 
 
-mongoose.connect("mongodb://127.0.0.1:27017",{
-    dbName:"backendapi",
-})
-.then(()=>console.log("Database Connected"))
-.catch((e)=>console.log(e))
 
 
 app.get("/",(req,res)=>{
     res.send("Nice working")
 })
 
-
-app.listen(5000,()=>{
-    console.log("Server is working")
-})
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ import { createUser, getAllUsers, getUserById } from "../controllers/user.js";
 const router = express.Router();
 
 
-router.get("all/:id",getUserById)
+router.get("/all/:id",getUserById)
 
 router.get("/all",getAllUsers)
 
